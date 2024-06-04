25eaaf9398e0b161f80e17a42d427464273162d5
ajayyy12 <0j0y1512@gmail.com>
2024-06-04 20:47:53 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Ajay"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Ajay SS"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "AS"
 )
 
 var ID ids.ID
